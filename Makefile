7519fd1e46698fb96492ebc08247d6abb990982a
Jonathan Campbell <jonathan@castus.tv>
2018-09-26 14:24:55 -0700

    Revert "start from here"
    This reverts commit af2977aaf2248a05c53ffca5fa357672c46b5d4a.

--- Makefile
+++ Makefile
@@ -1,35 +1,12 @@
-OBJ=o
-LIB=a
 
-HAXCC=haxcc
-# haxcc.OBJ must come LAST, must make cparsb.o and cparsl.o first
-HAXCC_OBJ=cparsb.$(OBJ) cparsl.$(OBJ) haxcc.$(OBJ)
+# hack for my sys
+CXX=/usr/gcc-4.8/bin/g++
 
-TARGETS=$(HAXCC)
+all: opcc
 
-$(HAXCC): $(HAXCC_OBJ)
-	gcc -o $@ $^
-
-all: $(TARGETS)
+opcc: opcc.cpp
+	$(CXX) -Wall -Wextra -pedantic -std=gnu++11 -o $@ $<
 
 clean:
-	rm -vf cparsb.c cparsb.c.h cparsl.c cparsl.c.h
-	rm -vf *.$(OBJ) *.$(LIB)
-	rm -vf $(TARGETS)
-	rm -vf *~
-
-.l.c: .SECONDARY
-	flex --header-file="$@.h" -o $@ $^
-
-.y.c: .SECONDARY
-	bison "--defines=$@.h" -o $@ $^
-
-.c.o:
-	gcc -std=gnu99 -DLINUX -DYYDEBUG=1 -DYYERROR_VERBOSE=1 -DYYTOKEN_TABLE=1 -Wall -pedantic -g3 -O0 -c -o $@ $<
-
-tests: $(HAXCC)
-	./$(HAXCC) <test1.c
-
-vtests: $(HAXCC)
-	valgrind --leak-check=full --show-reachable=yes --track-origins=yes ./$(HAXCC) <test1.c
+	rm -f opcc
 
---
+++ opcc.cpp
@@ -0,0 +1,251 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <string>
+
+enum tokentype_t {
+    TOK_NONE=0,
+    TOK_UINT,
+    TOK_INT,
+    TOK_FLOAT,
+    TOK_STRING,
+    TOK_ERROR,
+    TOK_MINUS,
+    TOK_SEMICOLON,
+
+    TOK_MAX
+};
+
+const char *tokentype_str[TOK_MAX] = {
+    "none",
+    "uint",
+    "int",
+    "float",
+    "string",
+    "error",
+    "minus",
+    "semicolon"
+};
+
+struct tokenstate_t {
+    enum tokentype_t    type;
+    std::string         string;
+    union {
+        uint64_t        u;
+        int64_t         i;
+    } intval;
+    long double         floatval;
+
+    inline const char *type_str(void) const {
+        return tokentype_str[type];
+    }
+} tokenstate;
+
+FILE*           srcfp = NULL;
+std::string     srcfile;
+int             untoke = -1;
+
+void untokechar(int c) {
+    assert(untoke < 0);
+    untoke = c;
+}
+
+int tokechar(void) {
+    unsigned char c;
+
+    if (untoke >= 0) {
+        c = (unsigned char)untoke;
+        untoke = -1;
+        return c;
+    }
+
+    if (srcfp == NULL) return -1;
+    if (feof(srcfp) || ferror(srcfp)) return -1;
+    if (fread(&c,1,1,srcfp) != 1) return -1;
+    return c;
+}
+
+bool toke(tokenstate_t &tok) {
+    int chr;
+
+    chr = tokechar();
+    while (chr == '\t' || chr == '\n' || chr == '\r' || chr == ' ') chr = tokechar();
+    if (chr < 0) return false;
+
+    switch ((unsigned char)chr) {
+        case '-': tok.type = TOK_MINUS;         return true;
+        case ';': tok.type = TOK_SEMICOLON;     return true;
+        default:
+            break;
+    };
+
+    if (chr == '\"') { /* it's a string */
+        tok.type = TOK_STRING;
+        tok.string.clear();
+
+        do {
+            chr = tokechar();
+            if (chr < 0) {
+                tok.type = TOK_ERROR;
+                return false;
+            }
+            if (chr == '\"') break;
+
+            if (chr == '\\') {
+                chr = tokechar();
+                if (chr == '\\' || chr == '\'' || chr == '\"') {
+                    tok.string += (char)chr;
+                }
+                else {
+                    tok.type = TOK_ERROR;
+                    return false;
+                }
+            }
+            else {
+                tok.string += (char)chr;
+            }
+        } while (true);
+
+        return true;
+    }
+    else if (isdigit((char)chr)) {
+        tok.type = TOK_UINT;
+        tok.string = (char)chr;
+        tok.intval.u = 0;
+
+        if (chr == '0') {
+            chr = tokechar();
+            if (chr == 'x') {
+                /* hexadecimal */
+                tok.string += (char)chr;
+
+                chr = tokechar();
+                if (!isxdigit(chr)) {
+                    tok.type = TOK_ERROR;
+                    return false;
+                }
+
+                tok.string += (char)chr;
+
+                do {
+                    chr = tokechar();
+                    if (!isxdigit(chr)) {
+                        untokechar(chr);
+                        break;
+                    }
+
+                    tok.string += (char)chr;
+                } while (true);
+
+                tok.intval.u = strtoull(tok.string.c_str(),NULL,0);
+                return true;
+            }
+            else if (isdigit(chr)) {
+                /* octal */
+                tok.string += (char)chr;
+            }
+            else {
+                untokechar(chr);
+                return true;
+            }
+        }
+
+        /* decimal */
+        do {
+            chr = tokechar();
+            if (chr == '.' && tok.type == TOK_UINT) {
+                tok.type = TOK_FLOAT;
+                tok.floatval = 0;
+            }
+            else if (!isdigit(chr)) {
+                untokechar(chr);
+                break;
+            }
+
+            tok.string += (char)chr;
+        } while (true);
+
+        if (tok.type == TOK_UINT)
+            tok.intval.u = strtoull(tok.string.c_str(),NULL,0);
+        else if (tok.type == TOK_FLOAT)
+            tok.floatval = strtof(tok.string.c_str(),NULL);
+        else
+            abort();
+
+        return true;
+    }
+
+    tok.type = TOK_ERROR;
+    return false;
+}
+
+int parse_argv(int argc,char **argv) {
+    char *a;
+    int i;
+
+    for (i=1;i < argc;) {
+        a = argv[i++];
+
+        if (*a == '-') {
+            do { a++; } while (*a == '-');
+
+            if (!strcmp(a,"i")) {
+                a = argv[i++];
+                if (a == NULL) return 1;
+                srcfile = a;
+            }
+            else {
+                fprintf(stderr,"Unknown sw %s\n",a);
+                return 1;
+            }
+        }
+        else {
+            fprintf(stderr,"Unexpected arg %s\n",a);
+            return 1;
+        }
+    }
+
+    if (srcfile.empty()) {
+        fprintf(stderr,"Source file -i required\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc,char **argv) {
+    if (parse_argv(argc,argv))
+        return 1;
+
+    if ((srcfp=fopen(srcfile.c_str(),"r")) == NULL) {
+        fprintf(stderr,"Unable to open file '%s', %s\n",srcfile.c_str(),strerror(errno));
+        return 1;
+    }
+
+    {
+        tokenstate_t tok;
+
+        while (toke(/*&*/tok)) {
+            fprintf(stderr,"%s(%d)\n",tok.type_str(),tok.type);
+            if (tok.type == TOK_STRING) fprintf(stderr," \"%s\"\n",tok.string.c_str());
+            if (tok.type == TOK_UINT) fprintf(stderr," 0x%llx/%llu\n",(unsigned long long)tok.intval.u,(unsigned long long)tok.intval.u);
+            if (tok.type == TOK_INT) fprintf(stderr," 0x%llx/%lld\n",(signed long long)tok.intval.i,(signed long long)tok.intval.i);
+            if (tok.type == TOK_FLOAT) fprintf(stderr," %.30Lf\n",(long double)tok.floatval);
+        }
+
+        if (tok.type == TOK_ERROR)
+            fprintf(stderr,"Parse error\n");
+    }
+
+    fclose(srcfp);
+    return 0;
+}
+
