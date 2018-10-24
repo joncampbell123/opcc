b45d51ab8cd7132b0b8437435e9111954bbb482c
Jonathan Campbell <jonathan@castus.tv>
2018-10-24 14:42:26 -0700

    amd-k6, syscall, begin

--- opcc.cpp
+++ opcc.cpp
@@ -5655,6 +5655,16 @@ int main(int argc,char **argv) {
         defines["cpuid"] = 1;
         defines["mmx"] = 1;
     }
+    else if (march == "amd-k6") {
+        if (fpuarch.empty())
+            fpuarch = "80587";
+
+        defines["vendor"] = "amd";
+        defines["cpulevel"] = 586;
+        defines["syscall"] = 1;
+        defines["cpuid"] = 1;
+        defines["mmx"] = 1;
+    }
     else if (march == "80686" || march == "686") {
         if (fpuarch.empty())
             fpuarch = "80687";
@@ -5685,6 +5695,7 @@ int main(int argc,char **argv) {
 
         defines["everything"] = 1;
         defines["cpulevel"] = 686;
+        defines["syscall"] = 1;
         defines["cpuid"] = 1;
         defines["cmov"] = 1;
         defines["mmx"] = 1;
